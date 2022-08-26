5db62a49b85aba59fa3f05f23aa96a3546226686
firesoul956 <FireSoul956@gmail.com>
2022-08-26 10:36:20 -0600

    added 3 questions cpp file.

---
+++ src/3questions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    double value;
+
+    std::cout <<"floating number please: ";
+    std::cin >> value;
+    std::cout<< "your vaule is: " <<value<< std::endl;
+    return 0;
+}
