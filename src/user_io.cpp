37ff92769e12079b9600f3f757a96f126c6886ea
firesoul956 <FireSoul956@gmail.com>
2022-08-30 22:48:22 -0600

    making sure I actually use parameters in 3questions.

--- src/user_io.cpp
+++ src/user_io.cpp
@@ -27,11 +27,11 @@ double GetDouble(std::istream& is, std::ostream& os,const std::string& prompt){
 int askQuestions3(std::istream& is, std::ostream& os){
 
     std::string stringprompt="What is your favorite color? ";
-    std::string stringvalue=getString(std::cin, std::cout,stringprompt);
+    std::string stringvalue=getString(is, os,stringprompt);
     std::string doubleprompt="What is your favorite integer? ";
-    int intvalue=GetDouble(std::cin, std::cout,doubleprompt);
+    int intvalue=GetDouble(is, os,doubleprompt);
     std::string intprompt="What is your favorite number? ";
-    double doublevalue=GetDouble(std::cin, std::cout,intprompt);
+    double doublevalue=GetDouble(is, os,intprompt);
     int i;
     std::string space=" ";
     for (i=0; i<intvalue;i++){
