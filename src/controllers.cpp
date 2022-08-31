70a9ffb6e219b324f52cab8d274a3071338aef60
firesoul956 <FireSoul956@gmail.com>
2022-08-30 22:31:34 -0600

    try try again.

--- src/controllers.cpp
+++ src/controllers.cpp
@@ -1,6 +1,6 @@
 #include "imagemenu.h"
 
 int assignment1( std::istream& is, std::ostream& os ){
-    int value = askQuestions3(std::cin, std::cout);
+    int value = askQuestions3(is, os);
     return value;
 }
