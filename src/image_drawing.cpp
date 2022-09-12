40e85868872eee8b6328a8f9ba68d2c21f0acc30
firesoul956 <FireSoul956@gmail.com>
2022-09-12 11:23:39 -0600

    lets hope its fixed.

--- src/image_drawing.cpp
+++ src/image_drawing.cpp
@@ -6,6 +6,97 @@ void stripedDiagonalPattern( std::istream& is, std::ostream& os, PPM& p ){
 
     std::string imagestringprompttwo = "Image width? ";
     int width = getInteger(is, os, imagestringprompttwo);
+
+    int rowhalf = (p.getHeight() / 2);
+    int colhalf = (p.getWidth() / 2);
+
+    int maxCV= height * width;
+    if (maxCV >255){
+        maxCV=255;
+    }
+    int row;
+    for (row=0; row < p.getHeight(); row++)
+    {
+        
+        int column;
+        for (column=0; column < p.getWidth(); column++)
+        {
+            
+            if (row < rowhalf && column < colhalf)
+            {
+                // top left
+                p.setChannel(row, column, 0, 0);
+                // int red =0;
+                if (column < row){
+                    p.setChannel(row, column, 2, 0);
+                }
+                else{
+                    p.setChannel(row, column, 2, 255);
+                }
+                // int blue =0;
+                p.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
+                // int = green ( 2*row + 2*column ) % 256;
+            }
+            else if (row >= rowhalf && column < colhalf)
+            {
+                // top right
+                // int red = 0;
+                p.setChannel(row, column, 0, 0);
+                // int blue =255;
+                if (column < row){
+                    p.setChannel(row, column, 2, 0);
+                }
+                else{
+                    p.setChannel(row, column, 2, 255);
+                }
+                // int = green ( 2*row + 2*column ) % 256;
+                p.setChannel(row, column, 1, (row + width - column - 1) % (p.getMaxColorValue()+1));
+            }
+            else if (row < rowhalf && column >= colhalf)
+            {
+                
+                // bottom left
+                // int red = 255;
+                if (row / 3  == 0){
+                    p.setChannel(row, column, 0, 0);
+                }
+                else{
+                    p.setChannel(row, column, 0, 255);
+                }
+                // int blue =0;
+                if (column < row){
+                    p.setChannel(row, column, 2, 0);
+                }
+                else{
+                    p.setChannel(row, column, 2, 255);
+                }
+                
+                // int = green ( 2*row + 2*column ) % 256;
+                p.setChannel(row, column, 1, (row + width - column - 1) % (p.getMaxColorValue()+1));
+            }
+            else if (row >= rowhalf && column >= colhalf)
+            {
+                // bottom right
+                // int red = 255;
+                if (row/3 ==0 ){
+                    p.setChannel(row, column, 0, 0);
+                }
+                else{
+                    p.setChannel(row, column, 0, 255);
+                }
+                // int blue =255;
+                if (column < row){
+                    p.setChannel(row, column, 2, 0);
+                }
+                else{
+                    p.setChannel(row, column, 2, 255);
+                }
+                // int = green ( 2*row + 2*column ) % 256;
+                p.setChannel(row, column, 1, (row + width - column - 1) % (p.getMaxColorValue()+1));
+            }
+           
+        }
+    }
 }
 
 
@@ -25,12 +116,14 @@ void diagonalQuadPattern(std::istream &is, std::ostream &os, Image &image)
     int rowhalf = (image.getHeight() / 2);
     int colhalf = (image.getWidth() / 2);
 
-    int row = 0;
-    for (row; row < image.getHeight(); row++)
+    int row;
+    for (row=0; row < image.getHeight(); row++)
     {
-        int column = 0;
-        for (column; column < image.getWidth(); column++)
+        
+        int column;
+        for (column=0; column < image.getWidth(); column++)
         {
+            
             if (row < rowhalf && column < colhalf)
             {
                 // top left
@@ -71,6 +164,7 @@ void diagonalQuadPattern(std::istream &is, std::ostream &os, Image &image)
                 // int = green ( 2*row + 2*column ) % 256;
                 image.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
             }
+           
         }
     }
 }
