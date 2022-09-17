2f773c490c6cb8a7b0c67e21b9ea7c695486355f
firesoul956 <FireSoul956@gmail.com>
2022-09-17 00:26:07 -0600

    messing with more stuff.

--- src/image_drawing.cpp
+++ src/image_drawing.cpp
@@ -184,43 +184,43 @@ void diagonalQuadPattern(std::istream &is, std::ostream &os, Image &image)
         for (column = 0; column < image.getWidth(); column++)
         {
 
-            if (row < rowhalf && column < colhalf)
+            if (row < rowhalf && column <= colhalf)
             {
                 // top left
                 image.setChannel(row, column, 0, 0);
                 // int red =0;
-                image.setChannel(row, column, 2, 255);
+                image.setChannel(row, column, 2, 0);
                 // int blue =0;
                 image.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
                 // int = green ( 2*row + 2*column ) % 256;
             }
-            else if (row >= rowhalf && column < colhalf)
+            else if (row >= rowhalf && column <= colhalf)
             {
                 // top right
                 // int red = 0;
                 image.setChannel(row, column, 0, 0);
                 // int blue =255;
-                image.setChannel(row, column, 2, 0);
+                image.setChannel(row, column, 2, 255);
                 // int = green ( 2*row + 2*column ) % 256;
                 image.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
             }
-            else if (row < rowhalf && column >= colhalf)
+            else if (row < rowhalf && column > colhalf)
             {
                 // bottom left
                 // int red = 255;
                 image.setChannel(row, column, 0, 255);
                 // int blue =0;
-                image.setChannel(row, column, 2, 255);
+                image.setChannel(row, column, 2, 0);
                 // int = green ( 2*row + 2*column ) % 256;
                 image.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
             }
-            else if (row >= rowhalf && column >= colhalf)
+            else if (row >= rowhalf && column > colhalf)
             {
                 // bottom right
                 // int red = 255;
                 image.setChannel(row, column, 0, 255);
                 // int blue =255;
-                image.setChannel(row, column, 2, 0);
+                image.setChannel(row, column, 2, 255);
                 // int = green ( 2*row + 2*column ) % 256;
                 image.setChannel(row, column, 1, (2 * row + 2 * column) % 256);
             }
