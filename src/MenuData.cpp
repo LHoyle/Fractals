c34c6e08974299676b2de78ad4dce260df04dade
firesoul956 <FireSoul956@gmail.com>
2022-10-06 19:21:48 -0600

    well its mostly working now. :X

--- src/MenuData.cpp
+++ src/MenuData.cpp
@@ -1,28 +1,31 @@
 #include "MenuData.h"
 
-typedef void (*ActionFunctionType)(ActionData &action_data){
-    std::vector<std::string>;
-    std::map<std::string,ActionFunctionType>;
-    std::map<std::string,std::string>;
-
-}
+typedef void (*ActionFunctionType)(ActionData& action_data);
 MenuData::MenuData()
 {
 }
-void MenuData::addAction(const std::string &name, ActionFunctionType func, const std::string &description)
+void MenuData::addAction(const std::string& name, ActionFunctionType func, const std::string &description)
 {
-    if name in cmds{
-        return false
+   cmds.push_back(name);
+   AFT[name]=func;
+   mapstring[name]=description;
+}
+const std::vector<std::string>& MenuData::getNames() const
+{
+    return cmds;
+}
+ActionFunctionType MenuData::getFunction(const std::string& name)
+{
+    if (AFT.find(name) ){
+        return AFT.find(name);
     }
-    else 
-    cmds.append (name,func)
+    return 0;
 }
-const std::vector<std::string> MenuData::getNames() const
+const std::string& MenuData::getDescription(const std::string& name)
 {
-}
-ActionFunctionType MenuData::getFunction(const std::string &name)
-{
-}
-const std::string MenuData::getDescription(const std::string &name)
-{
+    static std::string lolnope = "";
+    if (mapstring.find(name)){
+        return mapstring.find(name);
+    }
+    return lolnope;
 }
--- src/MenuData.h
+++ src/MenuData.h
@@ -12,9 +12,9 @@ public:
     typedef void (*ActionFunctionType)(ActionData& action_data);
     MenuData();
     void addAction(const std::string &name, ActionFunctionType func, const std::string& description);
-    const std::vector<std::string> &getNames() const;
+    const std::vector<std::string>& getNames() const;
     ActionFunctionType getFunction(const std::string& name);
-    const std::string &getDescription(const std::string& name);
+    const std::string& getDescription(const std::string& name);
 
 protected:
     std::vector<std::string> cmds;
