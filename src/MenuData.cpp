3688ad40e87484d8849a2358f46f5cf318c71d97
firesoul956 <FireSoul956@gmail.com>
2022-10-06 20:22:42 -0600

    getting help, this may work now.

--- src/MenuData.cpp
+++ src/MenuData.cpp
@@ -1,32 +1,42 @@
 #include "MenuData.h"
 
-typedef void (*ActionFunctionType)(ActionData& action_data);
+typedef void (*ActionFunctionType)(ActionData &action_data);
 MenuData::MenuData()
 {
 }
-void MenuData::addAction(const std::string& name, ActionFunctionType func, const std::string &description)
+void MenuData::addAction(const std::string &name, ActionFunctionType func, const std::string &description)
 {
-   cmds.push_back(name);
-   AFT[name]=func;
-   mapstring[name]=description;
+    cmds.push_back(name);
+    AFT[name] = func;
+    mapstring[name] = description;
 }
-const std::vector<std::string>& MenuData::getNames() const
+const std::vector<std::string> &MenuData::getNames() const
 {
     return cmds;
 }
-ActionFunctionType MenuData::getFunction(const std::string& name)
+ActionFunctionType MenuData::getFunction(const std::string &name)
 {
-     std::map<std::string, ActionFunctionType>::iterator AFTS= AFT.find(name) ;
-    if (AFTS!=AFT.end()){
+    //  std::map<std::string, ActionFunctionType>::iterator AFTS= AFT.find(name) ;
+    // if (AFTS!=AFT.end()){
+    //     return AFT[name];
+    // }
+    // return 0;
+
+    if (AFT.find(name) != AFT.end())
+    {
         return AFT[name];
     }
-    return 0;
+    else
+    {
+        return 0;
+    }
 }
-const std::string& MenuData::getDescription(const std::string& name)
+const std::string &MenuData::getDescription(const std::string &name)
 {
     static std::string lolnope = "";
-    std::map<std::string, std::string>::iterator found= mapstring.find(name);
-    if (found!=mapstring.end()){
+    std::map<std::string, std::string>::iterator found = mapstring.find(name);
+    if (found != mapstring.end())
+    {
         return mapstring[name];
     }
     return lolnope;
